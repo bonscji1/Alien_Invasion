56114de7ef9c85583b79ae057edd8cfe07120032
CraaYa <ji.bonsch@seznam.cz>
2020-07-07 14:29:13 +0200

    tweaking of ship

--- Ship.py
+++ Ship.py
@@ -11,7 +11,7 @@ class Ship(Sprite):
         self.screen_rect = game.screen.get_rect()
 
         #load ship image
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('images/ship.png')
         self.rect = self.image.get_rect()
 
         #start at mid bottom
